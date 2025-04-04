54793f8506d5bdcfadc8ad575f67267100811ea2
paulinacinque <pcinque1@mail.austral.edu.ar>
2025-04-04 12:27:41 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -4,3 +4,16 @@ def check_vowels():
 
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_in_string_test.py` o `python tp3_in_string_test.py`
+    name = "Paulina"
+    a = "a"
+    e = "e"
+    i = "i"
+    o = "o"
+    u = "u"
+    print(a in name)
+    print(e in name)
+    print(i in name)
+    print(o in name)
+    print(u in name)
+
+check_vowels()
